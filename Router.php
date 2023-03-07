1e565051fa692a0e36804b92c91b3020dcdf5d56
Radhames2418 <Radhamesenc2412002@gmail.com>
2023-03-07 14:26:56 -0400

    working in progress

--- Router.php
+++ Router.php
@@ -45,9 +45,16 @@ class Router
         ob_start(); 
 
         include_once __DIR__ . "/views/$view.php";
-
         $contenido = ob_get_clean(); // Limpia el Buffer
 
-        include_once __DIR__ . '/views/layout.php';
+        //Utilizar el layout de acuerdo a la url
+        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+
+        if (str_contains( $url_actual, '/admin')) {
+            include_once __DIR__ . '/views/admin-layout.php';
+        } else {
+            include_once __DIR__ . '/views/layout.php';
+        }
+
     }
 }
